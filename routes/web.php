f968d87870791b7b3ecb6f65c75bf6332520185b
JuanCarlosSandy <juan.sandy@uab.edu.bo>
2023-08-16 18:19:09 -0400

    Cambios de autenticazion brezee

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,38 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\EventoController;
 use App\Http\Controllers\PostController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('Bienvenido', function () {
-    // "hola a todos";
-    $noticias = [
-        ['id' => '1',
-        'titulo' => 'Vista pastoral',
-        'descripcion' => 'Pastor distrital llegara de visita '],
-        ['id' => '2',
-            'titulo' => 'Vista pastoral',
-            'descripcion' => 'Pastor distrital llegara de visita ']
-    ];
-    return view('noticia',['noticias' => $noticias ]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('Bienvenido/{id}', function ($id) {
-    return $id;
-});
+require __DIR__.'/auth.php';
 
-Route::get('tarea', function () {
-    // "hola a todos";
-    $personas  = [
-        ['nombre' => 'Valentin',
-        'edad' => '18'],
-        ['nombre' => 'kelly',
-        'edad' => '15']
-    ];
-    return view('personas',['personas' => $personas ]);
-});
+//============== TAREA =================
 
 Route::get('comment',[PostController::class, 'index']);
